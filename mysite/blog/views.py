a5136cf409d9b74850c77f827b99f2330ece3ab6
Hossam-Elden <hosam.elden@hotmail.com>
2018-02-15 16:42:46 +0200

    comments /templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
 from blog.models import Comment,Post
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -44,3 +46,43 @@ class DraftListView(LoginRequiredMixin,ListView):
 
     def get_queryset(self):
         return Post.object.filter(published_date__isnull=True).order_by('create_date')
+
+
+######################################################
+#Comments
+######################################################
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish
+    return redirect('post_detail',pk=pk)
+
+
+
+@login_required
+def add_comment_to_post(request,pk):
+    post= get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form  =CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post =post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form =CommentForm()
+    return  render(request,'blog/comment_form.html',{'form':form})
+
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
